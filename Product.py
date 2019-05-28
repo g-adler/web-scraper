1c44b9dcfedf42b9fbf88195ff2e77bfa614ff13
g-adler <guiadler@hotmail.com>
2019-05-28 00:38:40 -0300

    Criando classe Product e refinando scraper.py

---
+++ Product.py
@@ -0,0 +1,5 @@
+class Product:
+    def __init__(self, name, price, img):
+        self.name = name
+        self.price = price
+        self.img = img
--- scraper.py
+++ scraper.py
@@ -1,16 +1,35 @@
 #! /usr/bin/python3
 import requests
 import urllib.request
+from Product import Product
 from bs4 import BeautifulSoup
+import json
 
-url = 'https://nerdstore.com.br/categoria/especiais/game-of-thrones/'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+def getData():
 
-for product in soup.find_all('li', 'product'):
-    print('---------------------------------------------------------')
-    print(product.h2.string)
-    print('---------------------------------------------------------')
-    print(product.img.get('src'))
-    print('---------------------------------------------------------')
-    print(product.span.get('data-gtm4wp_product_price'))
+    products = []
+    url = 'https://nerdstore.com.br/categoria/especiais/game-of-thrones/'
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        raise Exception('Erro na busca de Informações: ' + str(response.status_code))
+
+    items = BeautifulSoup(response.text, 'html.parser')
+
+    for item in items.find_all('li', 'product'):
+        name = item.h2.string
+        price = item.span.get('data-gtm4wp_product_price')
+        img = item.img.get('src')
+        product = Product(name, price, img)
+        products.append(product)
+    if not products:
+        raise Execption('Erro na formação do Array')
+    return products
+
+def loadJson():
+
+    products = getData()
+    json_list = json.dumps([object.__dict__ for object in products])
+    return json_list
+
+loadJson()
